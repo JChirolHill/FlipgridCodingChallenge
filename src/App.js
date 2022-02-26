b43ad6b90b202d546617783db49e3a01bbe8e301
J Hill <chirolhi@usc.edu>
2022-02-26 15:51:28 -0800

    Added state, basic functionality implemented

--- src/App.js
+++ src/App.js
@@ -4,36 +4,51 @@ import Modal from './Components/Modal.js';
 import SignUpForm from './Components/SignUpForm.js';
 import Welcome from './Components/Welcome.js';
 import PageNotFound from './Components/PageNotFound.js';
+import React from 'react';
 
-export default function App() {
-    return (
-        <div className="container h-100">
-            <div className="row justify-content-center align-items-center h-100">
-                <Router>
-                    <Routes>
-                        <Route 
-                            path="/" 
-                            element={
-                                <Modal
-                                    headerFirst={"Let's"}
-                                    headerSecond={"Sign Up"}
-                                    info={"Use the form below to sign up for this super awesome service. You're only a few steps away!"}
-                                    content={<SignUpForm/>}/>
-                            }>
-                        </Route>
-                        <Route 
-                            path="welcome" 
-                            element={
-                                <Modal
-                                    headerFirst={"Welcome"}
-                                    headerSecond={"FIRSTNAME"}
-                                    info={"You have been registered for this awesome service. Please check your email listed below for instructions."}
-                                    content={<Welcome/>}/>
-                            }/>
-                        <Route path="*" element={<PageNotFound/>} />
-                    </Routes>
-                </Router>
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            userName: '',
+            userEmail: ''
+        };
+    }
+
+    updateUserInfo = (userName, userEmail) => {
+        this.setState({ userName, userEmail });
+    }
+
+    render() {
+        return (
+            <div className="container h-100">
+                <div className="row justify-content-center align-items-center h-100">
+                    <Router>
+                        <Routes>
+                            <Route 
+                                path="/" 
+                                element={
+                                    <Modal
+                                        headerFirst={"Let's"}
+                                        headerSecond={"Sign Up"}
+                                        info={"Use the form below to sign up for this super awesome service. You're only a few steps away!"}
+                                        content={<SignUpForm handleUpdateUserInfo={this.updateUserInfo}/>}/>
+                                }>
+                            </Route>
+                            <Route 
+                                path="welcome" 
+                                element={
+                                    <Modal
+                                        headerFirst={"Welcome"}
+                                        headerSecond={this.state.userName}
+                                        info={"You have been registered for this awesome service. Please check your email listed below for instructions."}
+                                        content={<Welcome email={this.state.userEmail}/>}/>
+                                }/>
+                            <Route path="*" element={<PageNotFound/>} />
+                        </Routes>
+                    </Router>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function SignUpForm() {
+export default function SignUpForm(props) {
     const navigate = useNavigate();
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+
+    const handleChangeUserName = (event) => {
+        setUserName(event.target.value);
+    }
+
+    const handleChangeUserEmail = (event) => {
+        setUserEmail(event.target.value);
+    }
 
     const handleSignUp = (event) => {
         event.preventDefault();
+        props.handleUpdateUserInfo(userName, userEmail);
         navigate('/welcome');
     }
 
@@ -12,12 +24,12 @@ export default function SignUpForm() {
         <form className="form">
             <div className="form-group">
                 <label htmlFor="form-fName" className="font-weight-bold">First Name</label>
-                <input type="text" id="form-fName" className="form-control fg-form-control fg-cc-border" required/>
+                <input type="text" id="form-fName" className="form-control fg-form-control fg-cc-border" onChange={handleChangeUserName} required/>
             </div>
 
             <div className="form-group">
                 <label htmlFor="form-email" className="font-weight-bold">Email Address</label>
-                <input type="email" id="form-email" className="form-control fg-form-control fg-cc-border" required/>
+                <input type="email" id="form-email" className="form-control fg-form-control fg-cc-border" onChange={handleChangeUserEmail} required/>
             </div>
             
             <div className="form-group">
